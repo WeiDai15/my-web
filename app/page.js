3a86e8e0085c9b92195905149a1942e0eaab2f12
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2024-08-19 17:18:08 -0600

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div style={{ display: "flex", justifyContent: 'center', backgroundColor:'black'}}>
+      <h1 style={{fontSize:'200px', color:'white'}}>Hello World!a</h1>
+    </div>
+  )
+}
